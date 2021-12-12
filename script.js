541ffae13cb8b4ff04cf89c8fbb83ec011e49ec0
johnmawoods <johnmawoods@yahoo.com>
2021-12-12 16:20:15 -0700

    updated nav scroll

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
-
-
-
+const about = document.querySelector('.about-page');
+const aboutBtn = document.querySelector('.about');
+const skills = document.querySelector('.skills-page');
+const skillsBtn = document.querySelector('.skills');
+const projects = document.querySelector('.projects-page');
+const projectsBtn = document.querySelector('.project');
+const contact = document.querySelector('.contact-page');
+const contactBtn = document.querySelector('.contact');
 
 const navSlide = () => {
     burger.addEventListener('click', () => {
@@ -24,5 +29,35 @@ const navSlide = () => {
     });
   
 }
+
+aboutBtn.addEventListener('click', function(){
+    about.scrollIntoView({
+        behavior: 'smooth',
+    });
+
+});
+
+skillsBtn.addEventListener('click', function(){
+    skills.scrollIntoView({
+        behavior: 'smooth',
+    });
+
+});
+
+projectsBtn.addEventListener('click', function(){
+    projects.scrollIntoView({
+        behavior: 'smooth',
+    });
+
+});
+
+contactBtn.addEventListener('click', function(){
+    contact.scrollIntoView({
+        behavior: 'smooth',
+    });
+
+});
+
+
 
 navSlide();
